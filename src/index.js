71f06cda69c0c06d647528bab13999ad7aeb51eb
KIRANA\kirana <kirana0701@gmail.com>
2022-05-18 23:35:27 +0700

    Step 1 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './Components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
